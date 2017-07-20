4ab8570cd6b6cf371e7ba79e3f85e3c64c5abbb7
Almuz <almuz@inbox.lv>
2017-07-20 15:00:28 +0300

    Next version, realised button with counter which counts how many times button pressed

--- components/logo.jsx
+++ components/logo.jsx
@@ -5,7 +5,7 @@ import Icon from './icon';
 class Logo extends React.Component{
   render(){
     return(
-        <h1 className="picogram-logo">Picogram</h1>
+        <h1 className="picogram-logo">Picogram {this.props.name}</h1>
 
 
     );
--- pages/main.jsx
+++ pages/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../components/logo';
 import Icon from '../components/icon';
 import Menu from '../components/menu';
+import Like from '../components/like';
 
 
 class Main extends React.Component{
@@ -10,14 +11,15 @@ class Main extends React.Component{
       <div className="container">
         <header className="header">
           <Icon />
-          <Logo />
+          <Logo name='Alex' />
         </header>
         <nav className="navigation">
           <Menu />
 
         </nav>
         <main className="content">
-          <img className="picogram-image" src="https://ih1.redbubble.net/image.195004521.4649/flat,800x800,075,f.u5.jpg"></img>
+          <img className="picogram-image" src="https://ih1.redbubble.net/image.195004521.4649/flat,800x800,075,f.u5.jpg" />
+          <Like />
 
         </main>
         <footer className="footer">
